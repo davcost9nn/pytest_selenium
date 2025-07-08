13982c1c12329a59effb81ccc244f94a88fbced2
unknown <davcost@yandex.ru>
2025-07-08 14:49:39 +0300

    Commit with time.sleep

--- test_items.py
+++ test_items.py
@@ -1,11 +1,18 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_items(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    time.sleep(30)
+
+    try:
+        button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+        assert button.is_displayed(), "Кнопка не видна!"
+    except NoSuchElementException:
+        pytest.fail("Кнопка 'Добавить в корзину' не найдена на странице!")
 
 
